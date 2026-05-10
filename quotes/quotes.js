af8ae8a4883db354b7b0824181c47796d83e03c0
Adonay Mendez <adonay.mendez255@gmail.com>
2026-05-10 10:02:22 -0400

    Add animations and hover effects to quotes child elements

--- quotes/quotes.js
+++ quotes/quotes.js
@@ -2,16 +2,20 @@ let quotesContainer = document.getElementById("quotesContainer");
 
 let quotes = [
   {
+    quote: `"Instead of asking? 'Am I good enough for this?', Try asking: 'Am I willing to be bad enough at this long enough to get good?'"`
+  },
+
+  {
+    quote: `"Start where you are, Use what you have, Do what you can!" - [Arthur Ashe]`
+  },
+
+  {
+    quote: `"The win is not in the size of the step. It's in the direction!" - [Oliver Wendell Holmes Jr.]`
+  },
+
+  {
     quote: `"There's a difference between settling, and wanting to be the best" - [All Might]`
-  },
-
-  {
-        quote: `"Start where you are, Use what you have, Do what you can!" - [Arthur Ashe]`
-  },
-
-  {
-    quote: `"The win is not in the size of the step. It's in the direction!" - [ChatGPT]`
-  },
+  }
 ]; 
 
 function addQuotesToPage(quotes){
