aea1e457ab4ec0c0802cf224eb70f58d973ecb9d
Adonay Mendez <adonay.mendez255@gmail.com>
2026-05-27 11:42:20 -0400

    appendChild to DOM + flex-wrap on goals-container

---
+++ goals/goals.js
@@ -0,0 +1,68 @@
+const goalsContainer = document.getElementById("goalsContainer"); 
+
+const goals = [
+  {
+    goal: "Exercise"
+  },
+  
+  {
+    goal: "Exercise"
+  },
+
+  {
+    goal: "Exercise"
+  },
+
+  {
+    goal: "Exercise"
+  },
+
+  {
+    goal: "Exercise"
+  },
+
+  {
+    goal: "Exercise"
+  },
+
+  {
+    goal: "Exercise"
+  },
+
+  {
+    goal: "Exercise"
+  },
+
+  {
+    goal: "Exercise"
+  },
+];
+
+// A function that when called upon will be added to the child of goalsContainer div in the html
+function addGoalsToPage (goals) {
+
+  // A for loop that will iterate through each array object individually starting at i = 0 for as long as it's less than the length of the array
+    for (i=0; i < goals.length; i++) {
+
+  // Assgining a created div element to the div variable 
+    let div = document.createElement("div");
+  
+  // Adding a class name to the previously div variable
+    div.classList.add("goals-row");
+
+    // Assigns goal to the current object of the array objects of goals being proccessed
+    const goal = goals[i]; 
+
+    // To add HTML code the div varible created previously 
+    // Applies to each object item individually
+    // div.innerHTML= `${goals.goal}`;
+
+
+    goalsContainer.appendChild(div);
+
+  }
+}
+
+addGoalsToPage(goals);
+
+
