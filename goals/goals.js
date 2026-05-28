cd6f988425b2cc0637e44d997510d757316bd0f2
Adonay Mendez <adonay.mendez255@gmail.com>
2026-05-28 12:28:44 -0400

    Add shadow to goals title

--- goals/goals.js
+++ goals/goals.js
@@ -1,65 +1,39 @@
 const goalsContainer = document.getElementById("goalsContainer"); 
 
-const goals = [
-  {
-    goal: "Exercise"
-  },
+//TODO: Add Completion Status + Comment + Shawdow
+
+const goals = [ 
   
   {
-    goal: "Exercise"
+    title: "Exercise"
   },
 
   {
-    goal: "Exercise"
+    title: "Exercise"
   },
+
 
   {
-    goal: "Exercise"
-  },
-
-  {
-    goal: "Exercise"
-  },
-
-  {
-    goal: "Exercise"
-  },
-
-  {
-    goal: "Exercise"
-  },
-
-  {
-    goal: "Exercise"
-  },
-
-];
-
-// A function that when called upon will be added to the child of goalsContainer div in the html
-function addGoalsToPage (goals) {
-
-  // A for loop that will iterate through each array object individually starting at i = 0 for as long as it's less than the length of the array
-    for (i=0; i < goals.length; i++) {
-
-  // Assgining a created div element to the div variable 
-    let div = document.createElement("div");
-  
-  // Adding a class name to the previously div variable
-    div.classList.add("goals-row");
-
-    // Assigns goal to the current object of the array objects of goals being proccessed
-    const goal = goals[i]; 
-
-    // To add HTML code the div varible created previously 
-    // Applies to each object item individually
-    div.innerHTML= `<p>${goal.goal}</p>`;
+    title: "Exercise"
+  }
+]; 
 
 
-    goalsContainer.appendChild(div);
+function addGoalsToPage(goals) {
 
+  for (let i=0; i < goals.length; i++) {
+
+    const div = document.createElement("div"); 
+
+    div.classList.add("goals-row"); 
+
+    const goal = goals[i];
+
+    div.innerHTML = `<p>${goal.title}</p>`
+
+    goalsContainer.appendChild(div); 
   }
 }
 
 addGoalsToPage(goals);
-
 
