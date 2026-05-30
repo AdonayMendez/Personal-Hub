5ae03b7cc4ecb26ab34c3fb4a53fde8b2f00b570
Adonay Mendez <adonay.mendez255@gmail.com>
2026-05-30 18:01:05 -0400

    Gaming: complete wantList section base version

--- gaming/gaming.js
+++ gaming/gaming.js
@@ -1,16 +1,35 @@
 const gamesContainer = document.getElementById("gamesContainer"); 
+const wantListGames = document.getElementById("wantListGames"); 
 
 const games = [
   {
-    title: "" ,
+    title: "Cod Mobile" ,
+    num: 1
+  },
+
+  {
+    title: "Real Flight Simulator",
+    num: 2
+
+  },
+
+  {
+    title: "112 Operator",
+    num: 3
+  },
+];
+
+const wantGames = [
+  {
+    title: "",
     num: 1
   },
 
   {
     title: "",
     num: 2
-
   },
+
 
   {
     title: "",
@@ -22,12 +41,11 @@ const games = [
     num: 4
   },
 
-  {
+    {
     title: "",
     num: 5
   }
 ];
-
 
 function addGamesToPage(games){
 
@@ -39,11 +57,36 @@ function addGamesToPage(games){
 
     const game = games[i]; 
 
-    div.innerHTML = `<p>${game.num}</p>`
-
+    div.innerHTML = `
+    <p>${game.num}</p>
+    <h1>${game.title}</h1>
+    `
     gamesContainer.appendChild(div);
-  }
-
+  } 
 }
 
+
+function addWantListGamesToPage(wantGames) {
+
+  for(let i=0; i < wantGames.length; i++) {
+
+    let div = document.createElement("div");
+
+    div.classList.add("wantList-games"); 
+
+    const game = wantGames[i];
+
+    div.innerHTML = 
+    `
+    <p>${game.num}</p>
+    <h1>${game.title}</h1>
+    `
+
+    wantListGames.appendChild(div);
+  }
+}
+
+
+
 addGamesToPage(games)
+addWantListGamesToPage(wantGames);
