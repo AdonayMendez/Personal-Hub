5a9c7e6fc566497f11b2c3b6cad726770390e85f
Adonay Mendez <adonay.mendez255@gmail.com>
2026-06-02 20:07:51 -0400

    Goals: basic modal pop-up test

--- goals/goals.js
+++ goals/goals.js
@@ -1,4 +1,7 @@
 const goalsContainer = document.getElementById("goalsContainer"); 
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn")
 
 //TODO: Name Goals + Optional: completion status 
 
@@ -55,3 +58,29 @@ function addGoalsToPage(goals) {
 addGoalsToPage(goals);
 
 
+function openModal() {
+  modal.classList.add("show");
+}
+
+function closeModal() {
+  modal.classList.remove("show");
+}
+
+openBtn.addEventListener("click", openModal); 
+closeBtn.addEventListener("click", closeModal);
+
+
+// Close when clicking outside the modal box
+openBtn.addEventListener("click", function(event) {
+  if (event.target === modal){
+  closeModal();
+  }
+});
+
+// close when pressing Escape 
+
+document.addEventListener("keydown", function(event){
+  if(event.key === "Escape"){
+    closeModal();
+  }
+})
