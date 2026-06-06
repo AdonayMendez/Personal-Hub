44169de1695898ec57b9c3fb65d10b5ff70e17ca
Adonay Mendez <adonay.mendez255@gmail.com>
2026-06-06 14:14:33 -0400

    books: add js function to display unique authorInfo

--- books/books.js
+++ books/books.js
@@ -1,14 +1,38 @@
 let bookShelf = document.getElementById("bookShelf"); 
+let bookInfo = document.getElementById("bookInfo"); 
+let authorName = document.getElementById("name"); 
+let authorAge = document.getElementById("age"); 
+let authorNationality = document.getElementById("nationality"); 
+let authorGenre = document.getElementById("genre"); 
+let wiltb = document.getElementById("wiltb"); 
+
 
 let currentBooks = [
   {
     title: '',
     img: '',
+    author: {
+      name: "John Mark Comer",
+      age:  45,
+      nationality: "American",
+      genre: "Christian Nonfiction, Spiritual Formation",
+      wiltb: "I like this book because it shows me how to take my time with many things.",
+      img: ""
+    }
+    
   },
 
   {
     title: '',
     img: '',
+    author: {
+      name: "jhd",
+      age:  45,
+      nationality: "American",
+      genre: "Christian Nonfiction, Spiritual Formation",
+      wiltb: "I like this book because it shows me how to take my time with many things.",
+      img: ""
+    }
 
   },
 
@@ -25,8 +49,22 @@ function addBooksToShelf(currentBooks){
 
     let div = document.createElement("div");
 
+    div.addEventListener('click', function(){
+      showBookInfo(currentBook);
+    });
+
     bookShelf.appendChild(div);
   }
 }
 
 addBooksToShelf(currentBooks);
+
+function showBookInfo(currentBook){
+  bookInfo.classList.add("show");
+
+  authorName.textContent = currentBook.author.name;
+  authorAge.textContent = ` Age: ${currentBook.author.age}`;
+  authorNationality.textContent = ` Nationality: ${currentBook.author.nationality}`;
+  authorGenre.textContent = ` Genre: ${currentBook.author.genre}`;
+  wiltb.textContent = `${currentBook.author.wiltb}`;
+}
