ef075436783f2fd712121948e5f0195adb4469ab
Adonay Mendez <adonay.mendez255@gmail.com>
2026-06-06 16:32:58 -0400

    books: unique author images for each selected currently reading book

--- books/books.js
+++ books/books.js
@@ -5,41 +5,50 @@ let authorAge = document.getElementById("age");
 let authorNationality = document.getElementById("nationality"); 
 let authorGenre = document.getElementById("genre"); 
 let wiltb = document.getElementById("wiltb"); 
+let authorPicture = document.getElementById("authorPic")
 
 
 let currentBooks = [
   {
     title: '',
-    img: '',
+    img: '../treoh-johnOrtberg.jpg',
     author: {
       name: "John Mark Comer",
       age:  45,
       nationality: "American",
       genre: "Christian Nonfiction, Spiritual Formation",
       wiltb: "I like this book because it shows me how to take my time with many things.",
-      img: ""
-    }
+      img: "../johnmarkc-1.webp"
+    },
     
   },
 
   {
     title: '',
-    img: '',
+    img: '../butHowDoItKnow-J.ClarkScott-1.jpg',
     author: {
-      name: "jhd",
-      age:  45,
+      name: "J. Clark Scott",
+      age: "61 (born April 6, 1964)",
       nationality: "American",
-      genre: "Christian Nonfiction, Spiritual Formation",
-      wiltb: "I like this book because it shows me how to take my time with many things.",
-      img: ""
-    }
-
+      genre: "Technology, Computer Science, Nonfiction",
+      wiltb: "Explains how computers work in a very simple manner",
+      img: "../J.clarkScott-1.jpg"
+    },
   },
 
   {
     title: '',
-    img: '',
+    img: '../despentes-vernonSubutex-1.avif',
+    author: {
+      name: "Virginie Despentes",
+      age: "56 (born June 13, 1969)",
+      nationality: "French",
+      genre: "Literary Fiction, Social Commentary, Feminist Literature",
+      wiltb: "The book provides a raw and realistic look at modern society while following interconnected characters through contemporary Paris.",
+      img: "../virginieDespentes-1.webp"
+    },    
   },
+
 ];
 
 function addBooksToShelf(currentBooks){
@@ -53,6 +62,9 @@ function addBooksToShelf(currentBooks){
       showBookInfo(currentBook);
     });
 
+    div.innerHTML = `
+    <img src = ${currentBook.img}>
+    `;
     bookShelf.appendChild(div);
   }
 }
@@ -67,4 +79,10 @@ function showBookInfo(currentBook){
   authorNationality.textContent = ` Nationality: ${currentBook.author.nationality}`;
   authorGenre.textContent = ` Genre: ${currentBook.author.genre}`;
   wiltb.textContent = `${currentBook.author.wiltb}`;
+
+  authorPicture.innerHTML = `
+    <img src = "${currentBook.author.img}">
+  
+  `;
+
 }
