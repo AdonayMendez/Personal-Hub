c36832ebdce116a0e9d35b37ca90b8ea81da94b9
Adonay Mendez <adonay.mendez255@gmail.com>
2026-06-08 15:42:19 -0400

    music: add window alert message for each row container

---
+++ music/music.js
@@ -0,0 +1,41 @@
+const spodifyOverlay = document.getElementById("spodifyOverlay");
+const albumOverlay = document.getElementById("albumOverlay");
+const writingOverlay = document.getElementById("writingOverlay");
+const completedOverlay = document.getElementById("completedOverlay");
+
+
+spodifyOverlay.addEventListener('click', function(){
+  showWritingAlert();
+});
+
+function showWritingAlert(){
+  alert("Nothing to see here! Private Info :)");
+}
+
+
+albumOverlay.addEventListener('click', function(){
+  showWritingAlert();
+});
+
+function showWritingAlert(){
+  alert("Nothing to see here! Private Info :)");
+}
+
+
+
+writingOverlay.addEventListener('click', function(){
+  showWritingAlert();
+});
+
+function showWritingAlert(){
+  alert("Nothing to see here! Private Info :)");
+}
+
+
+completedOverlay.addEventListener('click', function(){
+  showWritingAlert();
+});
+
+function showWritingAlert(){
+  alert("Nothing to see here! Private Info :)");
+}
